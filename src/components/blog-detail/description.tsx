ba8f8c1710675274db0ad74aacd95efaab3856b5
Lim Kit Siang <127509175+JihadZaidan@users.noreply.github.com>
2025-09-24 10:33:13 +0700

    fixing the animate for describe and image

--- src/components/blog-detail/description.tsx
+++ src/components/blog-detail/description.tsx
@@ -1,50 +1,70 @@
+"use client"
+
 import Image from "next/image"
 import Note from "../../../public/insight-blog/inblog-04.png"
 import Tims from "../../../public/one/landing-photos/branding.png"
 import Meeting from "../../../public/about/Video-Showcase.png"
+import { useSlideFromTop, useFadeIn, useSlideFromLeft, useSlideFromRight } from "@/library/animations"
+import { useRef } from "react"
 
 export default function DescribeDetail() {
+
+    const textRef = useRef<HTMLParagraphElement>(null);
+    const typoRef = useRef<HTMLDivElement>(null);
+    const NoteRef = useRef<HTMLImageElement>(null);
+    const TeamRef = useRef<HTMLImageElement>(null);
+    const MeetRef = useRef<HTMLImageElement>(null);
+
+    useSlideFromLeft(textRef, 0.3)
+    useSlideFromTop(NoteRef, 0.3)
+    useFadeIn(TeamRef, 0.3)
+    useSlideFromRight (typoRef, 0.3)
+    useFadeIn(MeetRef, 0.3)
+
     return (
         <div className="max-w-full w-full lg:px-20 lg:py-15 px-10 py-10">
             <div className="w-full gap-15 flex flex-col">
-                <p className="lg:w-[75%] text-[#595E5E] lg:text-3xl leading-[150%] font-sans font-medium">
-                    In todays rapidly evolving business landscape brands face 
-                    the constant challenge of staying relevant in the face of 
-                    changing market dynamics. Brand evolution the process of 
-                    adapting and transforming to meet shifting consumer needs 
-                    and market trends has become essential for businesses 
-                    seeking long-term success. By embracing brand evolution 
-                    companies can not only maintain their competitive edge but 
+                <p ref={textRef} className="lg:w-[75%] text-[#595E5E] lg:text-3xl leading-[150%] font-sans font-medium">
+                    In todays rapidly evolving business landscape brands face
+                    the constant challenge of staying relevant in the face of
+                    changing market dynamics. Brand evolution the process of
+                    adapting and transforming to meet shifting consumer needs
+                    and market trends has become essential for businesses
+                    seeking long-term success. By embracing brand evolution
+                    companies can not only maintain their competitive edge but
                     also foster growth and resilience in the face of uncertainty.
                 </p>
 
                 <div className="justify-right items-right lg:relative lg:mt-[-200px] lg:mb-[-700px] flex flex-col gap-10">
                     <Image
+                        ref={NoteRef}
                         src={Note}
                         alt="photos"
                         className="lg:relative lg:scale-[50%] lg:left-[200px]"
                     />
 
                     <Image
+                        ref={TeamRef}
                         src={Tims}
                         alt="branding"
                         className="relative lg:scale-[50%] lg:w-[70%] lg:top-[-550px] lg:left-50"
                     />
                 </div>
 
-                <div className="justify-right items-left flex flex-col lg:pl-120 gap-8">
+                <div ref={typoRef} className="justify-right items-left flex flex-col lg:pl-120 gap-8">
                     <h5 className="text-black lg:text-3xl text-xl font-sans font-medium text-left leading-[140%]">Empowering Businesses to Adapt and Thrive</h5>
                     <p className="text-[#595E5E] lg:text-2xl leading-[150%] font-sans font-medium">
-                        This blog post explores the intricacies of brand evolution and its significance in 
-                        modern business strategy. From identifying emerging market trends to implementing 
-                        effective adaptation strategies businesses can navigate the complexities of brand 
-                        evolution with confidence. Through insightful case studies actionable strategies 
+                        This blog post explores the intricacies of brand evolution and its significance in
+                        modern business strategy. From identifying emerging market trends to implementing
+                        effective adaptation strategies businesses can navigate the complexities of brand
+                        evolution with confidence. Through insightful case studies actionable strategies
                         and a focus on authenticity this post aims to equip brands with the knowledge and
                         tools they need to thrive in today&apos;s ever-changing marketplace.
                     </p>
                 </div>
 
                 <Image
+                    ref={MeetRef}
                     src={Meeting}
                     alt="discusion teams"
                     className="grayscale lg:aspect-[4/2] aspect-[2/1]"
