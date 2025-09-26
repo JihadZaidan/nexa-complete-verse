e393ffd7659f03001463d3d3fc3133fb9b76137a
Lim Kit Siang <127509175+JihadZaidan@users.noreply.github.com>
2025-09-26 11:24:54 +0700

    update new pages of is menu open

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,89 +1,40 @@
-'use client'
+"use client"
 
 import Link from "next/link"
-import { HiX } from "react-icons/hi"
 import { FiMenu } from "react-icons/fi"
-import { navigation } from "@/data/navigation"
-import { useState } from "react"
-import { ArrowRight } from "lucide-react"
+import { ArrowRight, DivideIcon } from "lucide-react"
 import { Button } from "../ui/button"
 import { useRef } from "react"
 import { useSlideFromTop } from "@/library/animations/useSlideFromTop"
-import Image from "next/image"
-import Snack from "../../../public/second/landing-page/header/chipschapa.png"
 
 export default function Navbar() {
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
-    const navRef = useRef<HTMLHeadElement>(null);
+  const navRef = useRef<HTMLHeadElement>(null)
+  useSlideFromTop(navRef, 0.35)
 
-    useSlideFromTop(navRef, 0.35);
+  return (
+    <header
+      ref={navRef}
+      className="relative z-50 w-full max-w-full mt-8 mb-20 lg:px-20 px-8"
+    >
+      <nav className="w-full flex justify-between items-center">
+        <h4 className="text-4xl font-normal font-sans text-black">Nexa</h4>
 
-    return (
-        <header
-            ref={navRef}
-            className="relative z-50 w-full max-w-full mt-8 mb-20 lg:px-20 px-8 justify-center items-center"
-        >
-            <nav className="w-full justify-between flex flex-row">
-                <div>
-                    <h4 className="text-4xl font-medium font-sans text-black">Nexa</h4>
-                </div>
-                <div className="flex flex-row gap-5 justify-center items-center">
-                    <Button className="flex flex-row justify-center gap-2">
-                        <p className="text-black text-lg font-medium lg:block hidden">Let&apos;s talk</p>
-                        <ArrowRight height={18} width={18} className="mt-[5px] text-black lg:block hidden" />
-                    </Button>
+        <div className="flex items-center gap-5">
+          <Button className="flex items-center gap-2">
+            <p className="text-black text-lg font-medium lg:block hidden">
+              Let&apos;s talk
+            </p>
+            <ArrowRight height={18} width={18} className="mt-[5px] text-black lg:block hidden" />
+          </Button>
 
-                    <div className="flex flex-row gap-5">
-                        <button
-                            className="text-black hover:text-black p-2 rounded-lg cursor-pointer"
-                            onClick={() => setIsMenuOpen(!isMenuOpen)}
-                        >
-                            {isMenuOpen ? (
-                                <HiX className="w-8 h-8" />
-                            ) : (
-                                <FiMenu className="w-8 h-8" />
-                            )}
-                        </button>
+          <DivideIcon className="text-neutral-900 lg:opacity-100 opacity-0 rotate-90" />
 
-                        {isMenuOpen && (
-                            <nav
-                                className="absolute z-[9999] top-16 left-0 w-full flex flex-col gap-20 bg-black shadow-lg pt-10 pb-12 lg:px-12 px-5"
-                            >
-                                <div className="flex lg:flex-row flex-col lg:justify-between gap-8">
-                                    <div className="lg:w-1/2 flex justify-center lg:justify-start lg:ml-[-110px] lg:opacity-[100%] opacity-0">
-                                        <Image
-                                            src={Snack}
-                                            alt="Snack assets"
-                                            className="rounded-md object-contain max-h-[280px]"
-                                        />
-                                    </div>
-                                    <div className="flex flex-col items-right gap-8">
-                                        {navigation
-                                            .filter((item) => !item.hideInNavbar && item.published)
-                                            .map((item) => (
-                                                <Link
-                                                    key={item.url}
-                                                    href={item.url}
-                                                    className="text-white hover:text-gray lg:text-3xl text-2xl font-sans font-medium lg:text-right text-left w-full"
-                                                >
-                                                    {item.label}
-                                                </Link>
-                                            ))}
-                                    </div>
-                                </div>
-                                <div className="flex lg:flex-row flex-col gap-8 lg:justify-between">
-                                    <h4 className="font-sans font-medium text-8xl text-neutral-100 text-left">Nexa</h4>
-                                    <div className="flex flex-col gap-5 pt-3">
-                                        <p className="text-neutral-100 font-sans font-normal lg:text-xl text-base lg:text-right text-left">123 Main Street, Anytown, USA 12345</p>
-                                        <p className="text-neutral-100 font-sans font-normal lg:text-xl text-base lg:text-right left">+1 141-756-9201 | contact@nexa.com</p>
-                                    </div>
-                                </div>
-                            </nav>
-                        )}
-                    </div>
-                </div>
-            </nav>
-        </header>
-    )
+          {/* Tombol ke halaman sidebar */}
+          <Link href="../sidebar" className="p-2">
+            <FiMenu className="w-8 h-8 text-black" />
+          </Link>
+        </div>
+      </nav>
+    </header>
+  )
 }
-
