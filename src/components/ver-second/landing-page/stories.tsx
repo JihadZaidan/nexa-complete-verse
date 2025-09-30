cd036e91cebfecd27bb6e92553ab6f209d7cc5a9
Lim Kit Siang <127509175+JihadZaidan@users.noreply.github.com>
2025-09-30 09:11:44 +0700

    feat(ui): added and improve Ref animate

--- src/components/ver-second/landing-page/stories.tsx
+++ src/components/ver-second/landing-page/stories.tsx
@@ -26,12 +26,12 @@ export default function StoryWork() {
         <div className="max-w-full w-full lg:px-20 lg:pt-20 lg:pb-5 px-5 py-14 bg-neutral-200">
             <div className="w-full lg:gap-26 gap-8 flex flex-col">
                 <div className="w-full flex lg:flex-row flex-col lg:justify-between lg:justify-left">
-                    <h3 className="font-sans font-normal text-neutral-800 lg:text-5xl text-3xl leading-[150%]">
+                    <h3 ref={headRef} className="font-sans font-normal text-neutral-800 lg:text-5xl text-3xl leading-[150%]">
                         Latest insights, stories, <br />
                         and news from Nexa.
                     </h3>
 
-                    <Button className="justify-center gap-[9px] lg:mt-22 mt-10 lg:ml-0 ml-[-150px]">
+                    <Button ref={btnRef} className="justify-center gap-[9px] lg:mt-22 mt-10 lg:ml-0 ml-[-150px]">
                         <p className="text-neutral-800 text-base font-normal">Explore the Insight</p>
                         <ArrowRight height={25} width={25} className="mt-[3px] text-neutral-800" />
                     </Button>
@@ -39,14 +39,14 @@ export default function StoryWork() {
 
                 <div className="w-full flex flex-col lg:gap-18 gap-12">
                     <div className="w-full flex lg:flex-row flex-col lg:gap-20 gap-12">
-                        <div className="w-full">
+                        <div ref={imcaRef} className="w-full">
                             <Image
                                 src={Canva}
                                 alt="business model canvas"
                                 className="lg:scale-[95%] grayscale"
                             />
                         </div>
-                        <div className="w-full lg:pt-30">
+                        <div ref={imdegRef} className="w-full lg:pt-30">
                             <Image
                                 src={UIUX}
                                 alt="business model canvas"
@@ -54,7 +54,7 @@ export default function StoryWork() {
                             />
                         </div>
                     </div>
-                    <div className="w-full lg:pl-10">
+                    <div ref={imbRef} className="w-full lg:pl-10">
                         <Image
                             src={Brand}
                             alt="business model canvas"
@@ -66,3 +66,4 @@ export default function StoryWork() {
         </div>
     )
 }
+
