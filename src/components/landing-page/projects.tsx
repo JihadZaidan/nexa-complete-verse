e5e8cd222f9d8cd01320832e3a0f406e0b2e04fe
Lim Kit Siang <127509175+JihadZaidan@users.noreply.github.com>
2025-10-08 13:14:49 +0700

    style(ui): styling position menus footer in tablet ui

--- src/components/landing-page/projects.tsx
+++ src/components/landing-page/projects.tsx
@@ -28,12 +28,12 @@ export default function Worksheet() {
   useSlideFromRight(tangoRef, 0.3);
 
   return (
-    <div className="max-w-full w-full lg:px-20 lg:py-36 py-14 px-5 gap-28 bg-white">
+    <div className="max-w-full w-full lg:px-20 lg:py-36 md:px-14 md:py-20 py-14 px-5 gap-28 bg-white">
       <div className="w-full flex flex-col gap-28">
-        <div className="w-full flex lg:flex-row flex-col justify-between gap-10">
+        <div className="w-full flex lg:flex-row md:flex-col flex-col justify-between gap-10">
           <h2
             ref={headRef}
-            className="lg:w-[40%] lg:text-5xl w-full text-left text-3xl font-normal font-sans text-black leading-[140%]"
+            className="lg:w-[40%] lg:text-5xl md:text-5xl w-full text-left text-3xl font-normal font-sans text-black leading-[150%]"
           >
             The work we do, and the people we help
           </h2>
@@ -46,9 +46,9 @@ export default function Worksheet() {
           </div>
         </div>
 
-        <div className="w-full flex flex-col gap-36">
+        <div className="w-full flex flex-col lg:gap-36 md:gap-24 gap-12">
           {/* Case 1 */}
-          <div className="w-full flex lg:flex-row flex-col gap-16">
+          <div className="w-full flex lg:flex-row md:flex-row flex-col gap-16">
             <div ref={firstRef} className="flex flex-col gap-6 bg-transparent">
               <h5 className="text-2xl font-normal text-left text-black">/ HOUSEL</h5>
               <h4 className="lg:text-4xl text-2xl font-normal text-left text-black">
@@ -59,19 +59,19 @@ export default function Worksheet() {
               </h4>
               <div>
                 <Button className="justify-start gap-[9px]">
-                  <p className="text-black text-xl font-normal text-left">View case study</p>
+                  <p className="text-black lg:text-xl md:text-lg text-base font-normal text-left">View case study</p>
                   <ArrowRight height={23} width={23} className="mt-[2px] text-black" />
                 </Button>
               </div>
             </div>
 
-            <div ref={figeRef} className="lg:w-[40%] w-full">
+            <div ref={figeRef} className="lg:w-[40%] md:w-[50%] w-full">
               <Image src={work1} alt="worker first photos" />
             </div>
           </div>
 
           {/* Case 2 */}
-          <div className="w-full flex lg:flex-row flex-col gap-16 justify-between">
+          <div className="lg:pl-[300px] w-full flex lg:flex-row md:flex-row flex-col gap-16 justify-start">
             <div ref={secondRef} className="flex flex-col gap-6 bg-transparent">
               <h5 className="text-2xl font-normal text-left text-black">/ CONTEXT AI</h5>
               <h4 className="lg:text-4xl text-2xl font-normal text-left text-black">
@@ -88,13 +88,13 @@ export default function Worksheet() {
               </div>
             </div>
 
-            <div ref={sagaRef} className="lg:w-[40%] w-full">
+            <div ref={sagaRef} className="lg:w-[40%] md:w-[50%] w-full">
               <Image src={work2} alt="worker second photos" />
             </div>
           </div>
 
           {/* Case 3 */}
-          <div className="w-full flex lg:flex-row-reverse flex-col-reverse gap-16 justify-between">
+          <div className="w-full flex lg:flex-row-reverse md:flex-row-reverse flex-col-reverse gap-16 justify-between">
             <div ref={thirdRef} className="flex flex-col gap-6 bg-transparent">
               <h5 className="text-2xl font-normal text-left text-black">/ CLOODY</h5>
               <h4 className="lg:text-4xl text-2xl font-normal text-left text-black">
@@ -108,7 +108,7 @@ export default function Worksheet() {
               </div>
             </div>
 
-            <div ref={tangoRef} className="lg:w-[55%] w-full">
+            <div ref={tangoRef} className="lg:w-[40%] md:w-[50%] w-full">
               <Image src={work3} alt="worker third photos" />
             </div>
           </div>
