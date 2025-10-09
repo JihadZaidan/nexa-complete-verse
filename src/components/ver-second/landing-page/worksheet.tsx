3bae4bcfe5e40438b0380558a84a62785966239d
Lim Kit Siang <127509175+JihadZaidan@users.noreply.github.com>
2025-10-09 13:02:40 +0700

    style(ui): styling padding in tablet ui improve and justify content

--- src/components/ver-second/landing-page/worksheet.tsx
+++ src/components/ver-second/landing-page/worksheet.tsx
@@ -18,34 +18,34 @@ export default function WorkerSheet() {
     useSlideFromLeft(imRef, 0.3)
 
     return (
-        <div className="max-w-full w-full bg-neutral-100 lg:px-20 lg:pt-25 lg:pb-30 px-8 py-8">
-            <div className="w-full flex flex-col lg:gap-20 gap-5">
+        <div className="max-w-full w-full bg-neutral-100 lg:px-20 lg:pt-25 lg:pb-30 md:px-14 md:py-16 px-8 py-8">
+            <div className="w-full flex flex-col lg:gap-20 md:gap-14 gap-5">
                 <div className="w-full flex flex-col gap-5">
-                    <h2 ref={headRef} className="font-sans font-normal lg:text-5xl text-2xl text-neutral-800 leading-[140%]">
+                    <h2 ref={headRef} className="font-sans font-normal lg:text-5xl md:text-4xl text-2xl text-neutral-800 leading-[140%]">
                         The work we do, <br />
                         and the people we help
                     </h2>
-                    <Button ref={btnRef} className="lg:hidden justify-center gap-[9px] ml-[-170px] ">
+                    <Button ref={btnRef} className="lg:hidden md:hidden justify-center gap-[9px] ml-[-170px] ">
                         <p className="text-neutral-800 text-base font-normal">All Case Study</p>
                         <ArrowRight height={20} width={20} className="mt-[3px] text-neutral-800" />
                     </Button>
                 </div>
 
-                <div className="w-full justify-left flex lg:flex-row-reverse flex-col-reverse gap-15">
+                <div className="w-full justify-left flex lg:flex-row-reverse md:flex-col flex-col-reverse gap-15">
                     <div className="w-full justify-left lg:ml-[-250px] flex flex-col lg:justify-between gap-8">
-                        <div ref={listRef} className="flex flex-col lg:gap-8 gap-4">
+                        <div ref={listRef} className="flex flex-col lg:gap-8 md:gap-6 gap-4">
                             <div className="flex flex-col gap-6">
-                                <h4 className="text-neutral-800 lg:text-4xl text-xl font-sans font-normal text-left">/ HOUSEL</h4>
-                                <p className="text-neutral-600 lg:text-3xl text-lg font-sans font-normal leading-[150%]">
+                                <h4 className="text-neutral-800 lg:text-4xl md:text-3xl text-xl font-sans font-normal text-left">/ HOUSEL</h4>
+                                <p className="text-neutral-600 lg:text-2xl md:text-lg text-lg font-sans font-normal leading-[150%]">
                                     Help you to sell, buy, 
-                                    mortgage,invest property, and  
+                                    mortgage,invest property, and <br /> 
                                     other real estate service.
                                 </p>
                             </div>
-                            <h4 className="text-neutral-500 lg:text-4xl font-sans font-normal text-left">/ CONTEXT AI</h4>
-                            <h4 className="text-neutral-500 lg:text-4xl font-sans font-normal text-left">/ CLODY</h4>
+                            <h4 className="text-neutral-500 lg:text-4xl md:text-2xl font-sans font-normal text-left">/ CONTEXT AI</h4>
+                            <h4 className="text-neutral-500 lg:text-4xl md:text-2xl font-sans font-normal text-left">/ CLODY</h4>
                         </div>
-                        <Button className="justify-center gap-[9px] lg:ml-[-250px] ml-[-170px]">
+                        <Button ref={btnRef} className="justify-center gap-[9px] lg:ml-[-250px] md:ml-[-580px] ml-[-170px]">
                             <p className="text-neutral-800 lg:text-xl text-base font-normal">All Case Study</p>
                             <ArrowRight height={20} width={20} className="mt-[3px] text-neutral-800" />
                         </Button>
@@ -55,12 +55,10 @@ export default function WorkerSheet() {
                         ref={imRef}
                         src={Work}
                         alt="project"
-                        className="lg:w-[55%] mr-[350px]"
+                        className="lg:w-[55%] lg:mr-[350px]"
                     />
                 </div>
             </div>
         </div>
     )
 }
-
-
