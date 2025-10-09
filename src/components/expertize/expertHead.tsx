7bf9418749ff7284ef72e0f8b273f8f2b830b857
Lim Kit Siang <127509175+JihadZaidan@users.noreply.github.com>
2025-10-09 09:01:42 +0700

    style(ui): styling padding in tablet ui improve

--- src/components/expertize/expertHead.tsx
+++ src/components/expertize/expertHead.tsx
@@ -13,7 +13,7 @@ export default function ExpertWorks() {
     return (
         <div className="max-w-full w-full bg-white lg:px-20 lg:py-16 md:px-14 md:py-10 px-8 py-8">
             <div className="w-full flex flex-col lg:gap-30 gap-20">
-                <h1 ref={headerRef} className="text-black font-sans font-normal lg:text-4xl text-2xl leading-[150%]">
+                <h1 ref={headerRef} className="text-black font-sans font-normal lg:text-4xl md:text-3xl text-2xl leading-[150%]">
                     — Discover Our Expertise: <br/>
                     Crafting Digital Strategy, <br/>
                     Elevating Brands, Web <br/>
@@ -28,4 +28,5 @@ export default function ExpertWorks() {
     )
 
 }
+
 
