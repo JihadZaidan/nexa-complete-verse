a810d52a35cf916e91d2f44001356c393909ecbd
Lim Kit Siang <127509175+JihadZaidan@users.noreply.github.com>
2025-10-09 09:01:05 +0700

    style(ui): styling padding in tablet ui improve

--- src/components/expertize/expertHead.tsx
+++ src/components/expertize/expertHead.tsx
@@ -11,7 +11,7 @@ export default function ExpertWorks() {
     useFadeIn(coroRef, 0.3);
 
     return (
-        <div className="max-w-full w-full bg-white lg:px-20 lg:py-16 px-8 py-8">
+        <div className="max-w-full w-full bg-white lg:px-20 lg:py-16 md:px-14 md:py-10 px-8 py-8">
             <div className="w-full flex flex-col lg:gap-30 gap-20">
                 <h1 ref={headerRef} className="text-black font-sans font-normal lg:text-4xl text-2xl leading-[150%]">
                     — Discover Our Expertise: <br/>
@@ -28,3 +28,4 @@ export default function ExpertWorks() {
     )
 
 }
+
