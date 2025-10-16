864c1c745b7193e257314c2d7ed3981f6f24f00f
Lim Kit Siang <127509175+JihadZaidan@users.noreply.github.com>
2025-10-16 12:12:56 +0700

    feat(solving): resolve for white gap bug in primary landing

--- src/app/expertise/page.tsx
+++ src/app/expertise/page.tsx
@@ -3,9 +3,10 @@ import ExpertiseFAQ from "@/components/expertize/featuresFAQ";
 
 export default function Expertise() {
     return (
-        <div className="max-w-full">
+        <div className="max-w-full mt-32">
             <ExpertWorks/>
             <ExpertiseFAQ/>
         </div>
     )
+
 }
