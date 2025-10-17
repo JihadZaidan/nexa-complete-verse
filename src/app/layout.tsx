b5d1e45a838aa4d5c7751cef4a92444efbf3f24b
Lim Kit Siang <127509175+JihadZaidan@users.noreply.github.com>
2025-10-17 10:59:01 +0700

    undo routing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@
 import Link from "next/link"
 import { FiMenu } from "react-icons/fi"
 import { ArrowRight } from "lucide-react"
-import { Button } from "@/components/ui/button"
+import { Button } from "../ui/button"
 import { useRef } from "react"
 import { useSlideFromTop } from "@/library/animations/useSlideFromTop"
 
@@ -51,4 +51,5 @@ export default function Navbar() {
     </header>
   )
 }
+
 
