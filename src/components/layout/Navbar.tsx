3573fc194e702b0678b20a504416571feb185188
Lim Kit Siang <127509175+JihadZaidan@users.noreply.github.com>
2025-10-17 11:45:24 +0700

    Update Navbar.tsx

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -3,7 +3,7 @@
 import Link from "next/link"
 import { FiMenu } from "react-icons/fi"
 import { ArrowRight } from "lucide-react"
-import { Button } from "../ui/button"
+import { Button } from "@/components/ui/button"
 import { useRef } from "react"
 import { useSlideFromTop } from "@/library/animations/useSlideFromTop"
 
@@ -49,5 +49,6 @@ export default function Navbar() {
     </header>
   )
 }
+
 
 
