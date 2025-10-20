2edd5187df43420281649392a70ff2e490bfce4c
Lim Kit Siang <127509175+JihadZaidan@users.noreply.github.com>
2025-10-20 10:34:34 +0700

    redir: repaired Gradient Overlay in layout.tsx prime

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,49 +1,31 @@
-'use client'
 import localfont from "next/font/local"
 import "./globals.css";
-import { usePathname } from "next/navigation";
-import React from "react";
 import Navbar from "@/components/layout/Navbar";
 import Footer from "@/components/layout/Footer";
+import { ReactNode } from "react";
+import GradientOverlay from "@/components/layout/GradientOverlay";
 
 const openSauce = localfont({
   src: [
-    {
-      path: "/OpenSauceSans/OpenSauceSans-Regular.ttf",
-      weight: "400",
-      style: "normal"
-    },
+    { path: "/OpenSauceSans/OpenSauceSans-Regular.ttf", weight: "400", style: "normal" },
   ],
   variable: "--font-open-sauce",
-  display: "swap"
-})
+  display: "swap",
+});
 
-export default function Layout({
-  children,
-}: Readonly<{ children: React.ReactNode }>) {
-  const pathname = usePathname();
-  const isHomePage = pathname === "/";
-
+export default function Layout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body className={`${openSauce.className} bg-white`}>
-        <main className="w-full min-h-screen pt-32">
-          {/* Gradient overlay hanya untuk homepage */}
-          {isHomePage && (
-            <div className="relative w-full h-full">
-              <div className="absolute bg-gradient-custom inset-0 backdrop-blur-md h-[70vh] z-[-10]">
-                <div className="absolute w-full h-full shadow-blur z-[-5]" />
-              </div>
-            </div>
-          )}
+        <Navbar />
 
-          <Navbar />
-          <div className="w-full overflow-hidden">
-            {children}
-          </div>
-          <Footer />
+        <main className="w-full min-h-screen overflow-hidden pt-32">
+          <GradientOverlay />
+          {children}
         </main>
+
+        <Footer />
       </body>
     </html>
-  )
+  );
 }
