bc971955e7a50395f730fafc7c3f23338d5e0def
Lim Kit Siang <127509175+JihadZaidan@users.noreply.github.com>
2025-10-22 09:40:04 +0700

    added compiler for compile ui version

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // primary option for styled-components //
+  compiler: {
+    styledComponents: true,
+  },
+
+  // configuration other option to added //
 };
 
 export default nextConfig;
