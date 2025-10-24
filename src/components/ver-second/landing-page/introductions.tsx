e22bac6a4d509be3f72f9f8587b2c20195eabf1b
Lim Kit Siang <127509175+JihadZaidan@users.noreply.github.com>
2025-10-24 09:01:55 +0700

    repaired and added client running animate

--- src/components/ver-second/landing-page/introductions.tsx
+++ src/components/ver-second/landing-page/introductions.tsx
@@ -1,3 +1,4 @@
+"use client"
 import Image from "next/image"
 import { Button } from "@/components/ui/button"
 import { ArrowRight } from "lucide-react"
@@ -63,3 +64,4 @@ export default function IntroCover() {
         </div>
     )
 }
+
