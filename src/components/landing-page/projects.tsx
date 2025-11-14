662c73179e9cb9c481ca181435c6f5d9cc8ef8db
chesshira <127509175+JihadZaidan@users.noreply.github.com>
2025-11-14 09:12:36 +0700

    refactor(ui): setting positions work section in landing page version 1
    - Repair Justify & items 
    - Setting content on Figma Feedback

--- src/components/landing-page/projects.tsx
+++ src/components/landing-page/projects.tsx
@@ -52,7 +52,7 @@ export default function Worksheet() {
         <div className="flex flex-col w-full lg:gap-36 md:gap-24 gap-16">
           
           {/* Case 1 */}
-          <div className="flex lg:flex-row md:flex-row flex-col items-center gap-10">
+          <div className="flex lg:flex-row md:flex-row flex-col justify-left items-left gap-8">
             <div ref={firstRef} className="flex flex-col gap-6 bg-transparent">
               <h5 className="text-2xl font-normal text-left text-black">/ HOUSEL</h5>
               <h4 className="lg:text-4xl text-2xl font-normal text-left text-black leading-[140%]">
@@ -77,7 +77,7 @@ export default function Worksheet() {
           </div>
 
           {/* Case 2 */}
-          <div className="flex lg:flex-row-reverse md:flex-row-reverse flex-col-reverse items-end justify-end gap-8">
+          <div className="flex lg:flex-row-reverse md:flex-row-reverse flex-col-reverse lg:items-end lg:justify-end md:justify-end md:items-end justify-left gap-8">
             <div ref={sagaRef} className="lg:w-[45%] md:w-[50%] w-full overflow-hidden">
               <Image
                 src={work2}
@@ -101,7 +101,7 @@ export default function Worksheet() {
           </div>
 
           {/* Case 3 */}
-          <div className="flex lg:flex-row-reverse md:flex-row-reverse flex-col-reverse items-center justify-between gap-10">
+          <div className="flex lg:flex-row-reverse md:flex-row-reverse flex-col-reverse justify-left items-left gap-8">
             <div ref={thirdRef} className="flex flex-col gap-6 bg-transparent">
               <h5 className="text-2xl font-normal text-left text-black">/ CLOODY</h5>
               <h4 className="lg:text-4xl text-2xl font-normal text-left text-black leading-[140%]">
@@ -129,6 +129,7 @@ export default function Worksheet() {
     </section>
   )
 }
+
 
 
 
